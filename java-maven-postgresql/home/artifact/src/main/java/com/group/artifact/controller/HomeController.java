fccb674a6ba3e2b740a682f251d0dc99ee11bf84
Salah Kumara <saleh.kumara@zucchetti.it>
2025-03-17 17:39:56 +0100

    added new docker-compose for a node environment

---
+++ java-maven-postgresql/home/artifact/src/main/java/com/group/artifact/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.group.artifact.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping("/")
+    public String greet() {
+        return "Hello World!";
+    }
+}
